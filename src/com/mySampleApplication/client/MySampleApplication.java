7b7543506c6693bec8b9a6634b90d190ac570d3e
tung.nn83@gmail.com <tung.nn83@gmail.com>
2020-06-06 21:32:53 +0700

    Initial commit

---
+++ src/com/mySampleApplication/client/MySampleApplication.java
@@ -0,0 +1,22 @@
+package com.mySampleApplication.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>
+ */
+public class MySampleApplication implements EntryPoint {
+
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad(){
+        RootPanel.get().add(new Login());
+    }
+
+}
